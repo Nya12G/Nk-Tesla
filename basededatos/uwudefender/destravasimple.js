771e139265253c9ef53fb0c3ac9c7b0983a7a0ab
Nya12G <95195613+Nya12G@users.noreply.github.com>
2022-01-23 07:27:34 -0500

    Update destravasimple.js

--- basededatos/uwudefender/destravasimple.js
+++ basededatos/uwudefender/destravasimple.js
@@ -1,5 +1,5 @@
 const kuakerzzz = () => {
-        return `Marquen como leido el chat \n\n⚡ *UwU* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n⚡ *UwU* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n⚡ *UwU* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n⚡ *UwU* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》
+        return `Marquen como leido el chat \n\n⚡ *ｏｎｉ - ｃｈａｎ* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n⚡ *ｏｎｉ - ｃｈａｎ* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n⚡ *ｏｎｉ - ｃｈａｎ* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n⚡ *ｏｎｉ - ｃｈａｎ* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》
 `
 }
 
